33cc7f6bd351d7f40d88f7a5748ff625a21c8ead
Kevinr2 <kevinreve096@gmail.com>
2021-11-10 18:38:29 -0500

    Api pokemons

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { GetPokemon, GetPokemonDetailsByApi } from '../API/GetPokemon'
 
 export default function Pokedex() {
+    const [pokemons, setPokemons] = useState()
+
+    useEffect(() => {
+        (async () => {
+            await LoadPokemons();
+        })()
+    }, [])
+    const LoadPokemons = async () => {
+        try {
+            const response = await GetPokemon();
+            console.log(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <SafeAreaView>
             <View>
